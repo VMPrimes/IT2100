ffd5dd5028f201646cd5aabcc8c33b3d0d9fa865
Vickie Primes <xcrun git config --global user.email>
2021-01-28 09:45:14 -0500

    Lab 2

--- Lab2.playground/Contents.swift
+++ Lab2.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var a:Double = 43.88
+var b:Double = 108.99
+var c:Double = 14.22
+print("$\(a + b + c)")
+
+
+
+
+var arrayOfClasses: [String] = ["Swift", "Objectve-C", "HTML", "CSS",  "JavaScript"]
+
+print(arrayOfClasses)
---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello, Swift"
+let constSTR = str
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)

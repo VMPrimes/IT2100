5d7d55e06794a5ecfc6fb739478842b310d803ce
Vickie Primes <xcrun git config --global user.email>
2021-01-28 09:51:06 -0500

    updated lab 3

---
+++ Lab-03/Lab-03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab-03
+//
+//  Created by Vickie Primes on 1/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPlayground examples.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var a: Double = 43.88
+var b: Double = 108.99
+var c: Double = 14.22
+
+
+typealias Dollars = Decimal
+let a =43.88Dollars(123456)
+let b = Dollars(1000)
+let c = a / b
+print(c)
+
+
+
+
+

9cfd52b818382396f1ae2eb704280b7bcb728dd5
Vickie Primes <xcrun git config --global user.email>
2021-01-28 21:21:57 -0500

    updated lab 2

--- Lab2.playground/Contents.swift
+++ Lab2.playground/Contents.swift
@@ -7,11 +7,24 @@ var b:Double = 108.99
 var c:Double = 14.22
 print("$\(a + b + c)")
 
-
-
+var isNegative: Bool
+a = -1
+if a < 0 {
+     print("It is less than 0")
+}
+else if a >= 0 {
+    print("It is greater than or = 0")
+}
 
 var arrayOfClasses: [String] = ["Swift", "Objectve-C", "HTML", "CSS",  "JavaScript"]
 
 print(arrayOfClasses)
+
+
+/* Optional values say either "there is a value, and it equals x" or "there isn't a value at all".*/
+
+//nil means that a variable(sometimes called an object) is empty and has no value assigned.
+
+
 
 

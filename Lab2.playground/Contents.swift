45c833b33afa1071bc17c382c2583d3582b7f8d4
Vickie Primes <xcrun git config --global user.email>
2021-02-01 20:41:25 -0500

    Final for Lab 2

--- Lab2.playground/Contents.swift
+++ Lab2.playground/Contents.swift
@@ -2,12 +2,12 @@ import UIKit
 
 var str = "Hello, playground"
 
-var a:Double = 43.88
+var a:Double = 43.88  // 1
 var b:Double = 108.99
 var c:Double = 14.22
 print("$\(a + b + c)")
 
-var isNegative: Bool
+var isNegative: Bool  // 2
 a = -1
 if a < 0 {
      print("It is less than 0")
@@ -16,14 +16,14 @@ else if a >= 0 {
     print("It is greater than or = 0")
 }
 
-var arrayOfClasses: [String] = ["Swift", "Objectve-C", "HTML", "CSS",  "JavaScript"]
+var arrayOfClasses: [String] = ["Swift", "Objectve-C", "HTML", "CSS",  "JavaScript"] // 3
 
 print(arrayOfClasses)
 
 
-/* Optional values say either "there is a value, and it equals x" or "there isn't a value at all".*/
+/* Optional values say either "there is a value, and it equals x" or "there isn't a value at all".*/  // 4
 
-//nil means that a variable(sometimes called an object) is empty and has no value assigned.
+//nil means that a variable(sometimes called an object) is empty and has no value assigned.  // 5
 
 var myReading1 = Float?(nil)  //6.
 var myReading2 = Float?(nil)
@@ -44,40 +44,29 @@ for value in 1...5 {
     print(value)
 }
 
-for (a) in "5,6 .. 10".enumerated() {
-    print("\(a)")
+//let String =  5...10<=numbersFavorite  // 8
+let numbersFavorite = [5,6,7,8,9,10]
+for ( index, number) in numbersFavorite.enumerated() {
+    //(5, "one"), (6, "two"), (7, "three"), (8, "four"), (9, "five"), (10, "six")
+    print("value at index \(index) is \(number)")
 }
-// Print  "5"
-// Print  "6"
-// Print  "7"
-// Print  "8"
-// Print  "9"
-// Print  "10"
 
 class greeter {
-    func sayHello(name: String, greeting: String) {
+    func sayHello(name: String, greeting: String) { // 9
         print("\(name), \(greeting)")
+        
     }
+    
 }
 let steve = greeter()
-steve.sayHello(name: "Vickie", greeting: " is mastering, the Swift programming language")
+    steve.sayHello(name: "Vickie", greeting: " is mastering, the Swift programming language")
+
 
 var m:Double = 43.88
 var n:Double = 108.99
 var o:Double = 14.22
 print("The total is")
     print("$\(m + n + o)")
-
-
-
-
-
-
-
-
-
-
-
 
 
 func getPerimeter(length: Int, width: Int) -> Int {  //10
@@ -87,29 +76,15 @@ print(getPerimeter(length: 7, width: 9))
     
 
 class dog {
- 
-    var dogBreed: String?
-    var dogName: String?
-    
-    func bark( name: String)  {
-            var bark: [String] = []
-     
-            if let dogBreed = poodle.dogBreed {
-                breed += [dogBreed]
-                poodle.dogBreed = "Poodle"
-            }
-     
-            if let dogName = trent.dogName {
-                name += [dogName]
-                trent.dogName = "Trent";                print("\(name) is barking!")
-                
-            }
-     
-    
-
-        }
-     
+    func barking(name: String, breed: String) { // 11
+        print("\(name), \(breed)")
+        
     }
+    
+}
+let tucker = dog()
+tucker.barking(name: "Tucker", breed: " the Poodle, is barking")
+
 
 
 

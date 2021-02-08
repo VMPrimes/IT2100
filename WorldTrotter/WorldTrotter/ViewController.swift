eabf09881ac0578c5093ea7cbb2d79a9c168e761
Vickie Primes <xcrun git config --global user.email>
2021-02-08 14:51:16 -0500

    Final Lab 3

--- WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -9,35 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewWillAppear( _ animated: Bool) {
-        super.viewWillAppear(animated)
-        createGradientLayer()
-    }
-    @IBOutlet weak var label: UILabel!
-        var gradientLayer = CAGradientLayer()
-    
-     
-
-        func createGradientLayer() {
-            gradientLayer = CAGradientLayer()
-            
-            gradientLayer.frame = self.view.bounds
-            
-            gradientLayer.colors = [UIColor.blue.cgColor, UIColor.green.cgColor, UIColor.yellow.cgColor,UIColor.red.cgColor]
+    override func viewDidLoad( ) {
+        super.viewDidLoad()
         
-             //fills the screen edge
-            UIColor.green.edgesIgnoringSafeArea(.allColors)
-            UIColor.blue.edgesIgnoringSafeArea(.allColors)
-            accessibilityUserInputLabels = textLabels
-           textLabels = ("212", "degrees Fahrenheit", "is really", "100", "degrees Celsius")
-            self.view.layer.addSublayer(gradientLayer)
-            let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-
-            let firstView = UIView(frame: firstFrame)
-             view.addSubview(firstView)        }
+        let newLayer = CAGradientLayer()
+        newLayer.colors = [ UIColor.blue.cgColor, UIColor.green.cgColor, UIColor.yellow.cgColor, UIColor.red.cgColor ]
+        newLayer.frame = view.frame
+        
+        view.layer.addSublayer(newLayer)
+        view.layer.insertSublayer(newLayer, at: 0)
         
     }
 
+}
 
 
 

24080928c7a01e61226ef72c7a75980bc69123db
Vickie Primes <xcrun git config --global user.email>
2021-02-28 17:09:17 -0500

    Lab06

---
+++ Lab06-TextInput/Lab06-TextInputTests/Lab06_TextInputTests.swift
@@ -0,0 +1,33 @@
+//
+//  Lab06_TextInputTests.swift
+//  Lab06-TextInputTests
+//
+//  Created by Vickie Primes on 2/25/21.
+//
+
+import XCTest
+@testable import Lab06_TextInput
+
+class Lab06_TextInputTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
